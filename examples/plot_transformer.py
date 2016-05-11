b25babd44bbf1dafb7f0fe7bd633c3814770aa62
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2016-05-10 21:42:24 -0400

    Made graphs look better

--- examples/plot_transformer.py
+++ examples/plot_transformer.py
@@ -9,13 +9,18 @@ import numpy as np
 from skltemplate import TemplateTransformer
 from matplotlib import pyplot as plt
 
-X = np.arange(50).reshape(-1, 1)
+X = np.arange(50, dtype=np.float).reshape(-1, 1)
+X /= 50
 estimator = TemplateTransformer()
 X_transformed = estimator.fit_transform(X)
 
-plt.plot(X.flatten()/X.max(), label='Original Data')
-plt.plot(X_transformed.flatten()/X_transformed.max(), label='Transformed Data')
-plt.title('Scaled plots of original and transformed data')
+plt.plot(X.flatten(), label='Original Data')
+plt.plot(X_transformed.flatten(), label='Transformed Data')
+plt.title('Plots of original and transformed data')
 
 plt.legend(loc='best')
+plt.grid(True)
+plt.xlabel('Index')
+plt.ylabel('Value of Data')
+
 plt.show()
