b452a018970790e3c01f132ed2149fd17814b195
ngoix <goix.nicolas@gmail.com>
2017-09-15 11:49:05 +0200

    up examples

--- examples/plot_fraud2rules.py
+++ examples/plot_fraud2rules.py
@@ -15,44 +15,42 @@ each new samples how many rules have find it abnormal.
 """
 print(__doc__)
 
-print('aaa')
+import numpy as np
+import matplotlib.pyplot as plt
+from ftrules import FraudToRules
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from ftrules import FraudToRules
+rng = np.random.RandomState(42)
+n_inliers = 1000
+n_outliers = 50
 
-# rng = np.random.RandomState(42)
-# n_inliers = 1000
-# n_outliers = 50
+# Generate train data
+I = 0.5 * rng.randn(n_inliers / 2, 2)
+X_inliers = np.r_[I + 2, I - 2]
+O = 0.5 * rng.randn(n_outliers, 2)
+X_outliers = O  # np.r_[O, O + [2, -2]]
+X_train = np.r_[X_inliers, X_outliers]
+y_train = [0] * n_inliers + [1] * n_outliers
 
-# # Generate train data
-# I = 0.5 * rng.randn(n_inliers / 2, 2)
-# X_inliers = np.r_[I + 2, I - 2]
-# O = 0.5 * rng.randn(n_outliers, 2)
-# X_outliers = O  # np.r_[O, O + [2, -2]]
-# X_train = np.r_[X_inliers, X_outliers]
-# y_train = [0] * n_inliers + [1] * n_outliers
+# fit the model
+clf = FraudToRules(random_state=rng, n_estimators=100)
+clf.fit(X_train, y_train)
 
-# # fit the model
-# clf = FraudToRules(random_state=rng, n_estimators=100)
-# clf.fit(X_train, y_train)
+# plot the line, the samples, and the nearest vectors to the plane
+xx, yy = np.meshgrid(np.linspace(-5, 5, 50), np.linspace(-5, 5, 50))
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
 
-# # plot the line, the samples, and the nearest vectors to the plane
-# xx, yy = np.meshgrid(np.linspace(-5, 5, 50), np.linspace(-5, 5, 50))
-# Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-# Z = Z.reshape(xx.shape)
+plt.title("Fraud To Rules")
+plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)
 
-# plt.title("Fraud To Rules")
-# plt.contourf(xx, yy, Z, cmap=plt.cm.Blues_r)
-
-# a = plt.scatter(X_inliers[:, 0], X_inliers[:, 1], c='white',
-#                 s=20, edgecolor='k')
-# b = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='red',
-#                 s=20, edgecolor='k')
-# plt.axis('tight')
-# plt.xlim((-5, 5))
-# plt.ylim((-5, 5))
-# plt.legend([a, b],
-#            ["inliers", "outliers"],
-#            loc="upper left")
-# plt.show()
+a = plt.scatter(X_inliers[:, 0], X_inliers[:, 1], c='white',
+                s=20, edgecolor='k')
+b = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='red',
+                s=20, edgecolor='k')
+plt.axis('tight')
+plt.xlim((-5, 5))
+plt.ylim((-5, 5))
+plt.legend([a, b],
+           ["inliers", "outliers"],
+           loc="upper left")
+plt.show()
