64a18dd64a610dea4c7d471d889aa80f915480a4
ngoix <goix.nicolas@gmail.com>
2017-09-19 16:16:42 +0200

    @RonanGautier review

--- ftrules/fraud2rules.py
+++ ftrules/fraud2rules.py
@@ -80,10 +80,10 @@ class FraudToRules(BaseEstimator):
     min_samples_split : int, float, optional (default=2)
         The minimum number of samples required to split an internal node for
         each decision tree.
-        - If int, then consider `min_samples_split` as the minimum number.
-        - If float, then `min_samples_split` is a percentage and
-          `ceil(min_samples_split * n_samples)` are the minimum
-          number of samples for each split.
+            - If int, then consider `min_samples_split` as the minimum number.
+            - If float, then `min_samples_split` is a percentage and
+              `ceil(min_samples_split * n_samples)` are the minimum
+              number of samples for each split.
 
     n_jobs : integer, optional (default=1)
         The number of jobs to run in parallel for both `fit` and `predict`.
@@ -105,10 +105,8 @@ class FraudToRules(BaseEstimator):
         trees) and further selected according to their respective OOB
         precisions and recalls.
         All rules fulfilling recall_min and precision_min conditions are saved.
-        A number n_rules of these rules are selected according to their OOB
-        precision.
-        The selected rules are used in the ``predict`` method and  can be
-        obtained with rules_[:n_estimators].
+        The n_estimators best rules are selected to be used in the ``predict``
+        method and  can be obtained with rules_[:n_estimators].
 
     estimators_ : list of DecisionTreeClassifier
         The collection of fitted sub-estimators used to generate candidate
@@ -168,12 +166,11 @@ class FraudToRules(BaseEstimator):
         ----------
         X : array-like, shape (n_samples, n_features)
             Training vector, where n_samples is the number of samples and
-            n_features is the number of features. XXX sparse matrix?
+            n_features is the number of features.
 
         y : array-like, shape (n_samples,)
             Target vector relative to X. Has to follow the convention 0 for
-            normal data, 1 for frauds.
-            XXX maybe make such y ourselves from input?
+            normal data, 1 for anomalies.
 
         sample_weight : array-like, shape (n_samples,) optional
             Array of weights that are assigned to individual samples, typically
@@ -200,9 +197,10 @@ class FraudToRules(BaseEstimator):
                              " class: %r" % self.classes_[0])
 
         if not set(self.classes_) == set([0, 1]):
-            warn("Found labels %s. This method assumes fraud to be labeled as"
-                 " 1 and normal data to be labeled as 0. Any label"
-                 " different from 0 will be considered as fraud."
+            warn("Found labels %s. This method assumes target class to be"
+                 " labeled as 1 and normal data to be labeled as 0. Any label"
+                 " different from 0 will be considered as being from the"
+                 " target class."
                  % set(self.classes_))
             y = (y > 0)
 
@@ -353,13 +351,13 @@ class FraudToRules(BaseEstimator):
         ----------
         X : array-like, shape (n_samples, n_features)
             The input samples. Internally, it will be converted to
-            ``dtype=np.float32`` XXX allow sparse matrix?
+            ``dtype=np.float32``
 
         Returns
         -------
-        is_inlier : array, shape (n_samples,)
-            For each observations, tells whether or not (+1 or -1) it should
-            be considered as an inlier according to the fitted model.
+        is_outlier : array, shape (n_samples,)
+            For each observations, tells whether or not (1 or 0) it should
+            be considered as an outlier according to the selected rules.
         """
 
         return np.array((self.decision_function(X) > 0), dtype=int)
@@ -368,7 +366,7 @@ class FraudToRules(BaseEstimator):
         """Average anomaly score of X of the base classifiers (rules).
 
         The anomaly score of an input sample is computed as
-        the negative weighted sum of the binary rules outputs. The weight is
+        the weighted sum of the binary rules outputs, the weight being
         the respective precision of each rule.
 
         Parameters
@@ -380,7 +378,7 @@ class FraudToRules(BaseEstimator):
         -------
         scores : array, shape (n_samples,)
             The anomaly score of the input samples.
-            The lower, the more abnormal. Negative scores represent outliers,
+            The higher, the more abnormal. Positive scores represent outliers,
             null scores represent inliers.
 
         """
@@ -463,12 +461,3 @@ class FraudToRules(BaseEstimator):
             return (-1, -1)
         pos = y[y > 0].sum()
         return y_detected.mean(), float(true_pos) / pos
-
-
-# if __name__ == '__main__':
-#     rnd = np.random.RandomState(0)
-#     X = 3 * rnd.uniform(size=(50, 5)).astype(np.float32)
-#     y = np.array([1] * (X.shape[0] - 10) + [0] * 10)
-#     clf = FraudToRules()
-#     clf.fit(X, y)
-#     clf.predict(X)
