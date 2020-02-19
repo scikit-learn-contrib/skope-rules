37a14e0ef44a5e02a91d58154a27523b3f5663e5
HOEBIN JEONG <31682226+stat17-hb@users.noreply.github.com>
2020-02-19 13:25:30 +0900

    Update skope_rules.py (fix invalid mask indexing for OOB samples)

--- skrules/skope_rules.py
+++ skrules/skope_rules.py
@@ -333,9 +333,9 @@ class SkopeRules(BaseEstimator):
                                                 self.estimators_features_):
 
             # Create mask for OOB samples
-#             mask = ~samples # samples is numpy array. when you put ~. it returns array([-(value_1 + 1), ..., -(value_n + 1)])
             mask = list(set(np.arange(len(samples))).difference(samples)
-            
+            # samples is numpy array. when you put ~. it returns array([-(value_1 + 1), ..., -(value_n + 1)])
+                        
             if sum(mask) == 0:
                 warn("OOB evaluation not possible: doing it in-bag."
                      " Performance evaluation is likely to be wrong"
