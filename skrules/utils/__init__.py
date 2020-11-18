8fc67a0104184ef3ab4a4add9f8350200b9aa6ec
fayeab <fayablaye@gmail.com>
2020-11-18 18:57:09 +0100

    feat: add function check_max_samples to utils/__init__.py

--- skrules/utils/__init__.py
+++ skrules/utils/__init__.py
@@ -3,6 +3,7 @@ from .checks import check_deduplication_criterion
 from .checks import check_custom_func
 from .checks import check_consistency
 from .checks import check_max_depth_duplication
+from .checks import check_max_samples
 from .util import get_confusion_matrix
 from .util import precision
 from .util import recall
